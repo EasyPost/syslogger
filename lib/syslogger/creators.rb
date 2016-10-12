5e09a0598e2e5a7bef018f2285b8539e74995919
Serhiy Rozum <serhiy@easypost.com>
2016-10-12 10:54:35 -0700

    Pass DGRAM socket as a parameter, not a block

--- lib/syslogger/creators.rb
+++ lib/syslogger/creators.rb
@@ -4,7 +4,7 @@ require 'socket'
 module SysLogger
   module Creators
     def self.unix_dgram_socket(socket_path)
-      proc {
+      Proc.new {
         client = Socket.new(Socket::Constants::AF_LOCAL, Socket::Constants::SOCK_DGRAM, 0)
         client.connect(Socket.pack_sockaddr_un(socket_path))
         client
@@ -12,7 +12,7 @@ module SysLogger
     end
 
     def self.unix_stream_socket(socket_path)
-      proc {
+      Proc.new {
         UnixSocket.new(socket_path)
       }
     end
--- lib/syslogger/logger.rb
+++ lib/syslogger/logger.rb
@@ -4,11 +4,11 @@ module SysLogger
   class Logger < MonoLogger
     attr_reader :logdev, :default_formatter
 
-    def initialize(logdev = nil, shift_age = 0, shift_size = 1048576, &block)
-      if logdev.nil? && block_given?
-        super(SysLogger::IO.new(&block), shift_age, shift_size)
+    def initialize(logdev = nil, shift_age = 0, shift_size = 1048576)
+      if logdev.is_a?(Proc)
+        super(SysLogger::IO.new(logdev), shift_age, shift_size)
       elsif logdev.nil?
-        super($stdout, shift_age, shift_size)
+        super(STDOUT, shift_age, shift_size)
       else
         super(logdev, shift_age, shift_size)
       end
