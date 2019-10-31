1d94a7a229027e6e1d4455de30566212017822a5
James Brown <jbrown@easypost.com>
2019-10-31 13:17:36 -0700

    make it easier to override appname or procid for syslogger
    Also, fixes an obnoxious bug where the procid was set at formatter
    construction time in a really hard-to-override way, and thus would be
    wrong for programs that do pre-fork importing. Oops.

--- spec/syslogger/formatter/rfc5424_spec.rb
+++ spec/syslogger/formatter/rfc5424_spec.rb
@@ -3,9 +3,16 @@ require 'date'
 
 describe SysLogger::Formatter::RFC5424 do
   its(:msgid)    { is_expected.to eq "-" }
-  its(:procid)   { is_expected.to eq Process.pid.to_s }
-  its(:appname)  { is_expected.to eq "-" }
+  its(:procid)   { is_expected.to eq nil }
+  its(:appname)  { is_expected.to eq nil }
   its(:facility) { is_expected.to eq 23 }
+
+  let(:test_date) { DateTime.new(2019, 10, 31, 20, 8) }
+  let(:test_hostname) { "host1sf" }
+
+  before do
+    allow(Socket).to receive(:gethostname).and_return(test_hostname)
+  end
 
   describe "#call" do
     it "generates Format" do
@@ -27,6 +34,34 @@ describe SysLogger::Formatter::RFC5424 do
         counter2 = 0
       end
       expect(counter2).to be > counter1
+    end
+
+    context "nil appname" do
+      it "formats correctly" do
+        expect(Socket).to receive(:gethostname).and_return(test_hostname)
+        expect(subject).to receive(:gen_xgroup).and_return(74901736)
+        expect(subject.call(::Logger::INFO, test_date, "Prog", "Message")).to eq (
+          "<190>1 2019-10-31T20:08:00.000000+00:00 host1sf - #{Process.pid.to_s} Prog [meta x-group=\"74901736\" x-counter=\"1\"] Message\n"
+        )
+      end
+    end
+
+    context "custom appname" do
+      it "formats correctly" do
+        subject.appname = "short-appname"
+        expect(subject).to receive(:gen_xgroup).and_return(74901736)
+        expect(subject.call(::Logger::INFO, test_date, "Prog", "Message")).to eq (
+          "<190>1 2019-10-31T20:08:00.000000+00:00 host1sf short-appname #{Process.pid.to_s} Prog [meta x-group=\"74901736\" x-counter=\"1\"] Message\n"
+        )
+      end
+
+      it "truncates to 48 bytes" do
+        subject.appname = "this-is-a-very-long-appname-with-many-parts-in-it-for-sure-yep"
+        expect(subject).to receive(:gen_xgroup).and_return(74901736)
+        expect(subject.call(::Logger::INFO, test_date, "Prog", "Message")).to eq (
+          "<190>1 2019-10-31T20:08:00.000000+00:00 host1sf this-is-a-very-long-appname-with-many-parts-in-it #{Process.pid.to_s} Prog [meta x-group=\"74901736\" x-counter=\"1\"] Message\n"
+        )
+      end
     end
   end
 end
--- spec/syslogger_spec.rb
+++ spec/syslogger_spec.rb
@@ -1,9 +1,28 @@
 describe SysLogger do
-  let(:logger) { SysLogger.new(StringIO.new) }
+  let(:io)     { StringIO.new }
+  let(:logger) { SysLogger.new(io) }
 
   describe '#new' do
     it 'creates a Logger' do
       expect(logger).to be_a SysLogger::Logger
+    end
+  end
+
+  describe "#appname" do
+    it 'returns nil if appname is nil' do
+      expect(logger.appname).to be_nil
+      logger.info('test')
+      expect(io.string).to match(
+        /<190>1.* #{Socket.gethostname} - #{Process.pid} - \[meta.*/
+      )
+    end
+
+    it 'overrides the appname' do
+      logger.appname = 'an-appname'
+      logger.info('test')
+      expect(io.string).to match(
+        /<190>1.* #{Socket.gethostname} an-appname #{Process.pid} - \[meta.*/
+      )
     end
   end
 end
